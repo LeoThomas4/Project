e3683a692274c94ece2122e04969620e6afaa58a
LeoThomas4 <720032@gmail.com>
2022-08-27 19:38:19 +0530

    Second code

--- src/test/java/com/git/GitClass.java
+++ src/test/java/com/git/GitClass.java
@@ -14,5 +14,17 @@ public class GitClass {
 		// TODO Auto-generated method stub
 
 	}
+	private void m4() {
+		// TODO Auto-generated method stub
+
+	}
+	private void m5() {
+		// TODO Auto-generated method stub
+
+	}
+	private void m6() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
